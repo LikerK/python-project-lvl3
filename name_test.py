01cb40949f02f38c0289de9843a3b68391a0fdcf
LikerK <90216934+LikerK@users.noreply.github.com>
2022-02-17 11:16:15 +0300

    Delete name_test.py

--- name_test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-a = '<link rel="stylesheet" media="all" href="/assets/application.css">'
-
-b = re.search(r'(?<=href="\/)([^\'"])*|(?<=src="\/)([^\'"])*', a)
-d = re.sub(r"\/", '-', b.group())
-print(d)
