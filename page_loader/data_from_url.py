842c7d3d5a903409896c555453661457df879db9
LikerK <nagorniykirill@gmail.com>
2022-03-26 22:24:01 +0300

    refactoring

--- page_loader/data_from_url.py
+++ page_loader/data_from_url.py
@@ -5,11 +5,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def download_file(url):
+def download_file(url, local_file=False):
     try:
         response = requests.get(url)
         response.raise_for_status()
     except requests.exceptions.RequestException as error:
-        logger.warning(f'{error} Status code: {requests.get(url).status_code}')
-        raise Exception(f'Status code {error}') from error
+        logger.warning(f'failed to download file from link {url}. Status code: {requests.get(url).status_code}')  # noqa: E501
+        if local_file is False:
+            raise Exception(f'Status code {error}') from error
     return response.content
